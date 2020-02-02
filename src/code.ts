a41726516035ea70845e4fde160f7baca4169658
V R <vr@Vs-MacBook-Pro.local>
2020-02-02 14:25:36 +0100

    Refactor

--- src/code.ts
+++ src/code.ts
@@ -1,45 +1,63 @@
 figma.showUI(__html__);
 
 figma.ui.onmessage = async msg => {
+
   if (msg.status === 200 && msg.response) {
 
     const response = await JSON.parse(msg.response);
-    const products = response.products;
-    const list = response.products.list;
-    const images = msg.images;
+    let list = response.products.list;
+    let images = msg.images;
+
+    function traverse(node, i) {
+      if (node.type === 'GROUP') {
+        for (const groupChild of node.children) traverse(groupChild, i);
+      }
+      else if (node.type === "TEXT" && node.name.indexOf("$") === 0) {
+        replaceText(node, i);
+
+      } else if (node.type === "RECTANGLE") {
+        replaceImage(node, i)
+      }
+    }
+
+    function closePlugin() {
+      figma.closePlugin();
+    }
+
+    async function replaceText(node, i) {
+      let nodeName = node.name.replace("$", "");
+      await figma.loadFontAsync(node.fontName);
+      node.characters = String(list[i][nodeName]);
+    }
+
+    function replaceImage(node, i) {
+      let data = images[i] as Uint8Array;
+      let imageHash = figma.createImage(new Uint8Array(data)).hash;
+
+      node.fills = [
+        { type: "IMAGE", scaleMode: "FILL", imageHash }
+      ];
+    }
 
     if (figma.currentPage.selection.length > 0) {
+      list.length = figma.currentPage.selection.length;
+
       let i = 0;
 
-      console.log('node', figma.currentPage);
       for (const node of figma.currentPage.selection) {
-        console.log('node', node);
 
         for (const childNode of node.children) {
-          if (childNode.type === "TEXT" && childNode.name.indexOf("$") === 0) {
-            let nodeName = childNode.name.replace("$", "");
-
-            await figma.loadFontAsync(childNode.fontName);
-            childNode.characters = list[i][nodeName];
-          } else if (childNode.type === "RECTANGLE") {
-
-            let data = images[i] as Uint8Array;
-            let imageHash = figma.createImage(new Uint8Array(data)).hash;
-
-            childNode.fills = [
-              { type: "IMAGE", scaleMode: "FILL", imageHash }
-            ];
-          }
+          traverse(childNode, i);
         }
 
         i++;
       }
 
-      // figma.closePlugin();
+      closePlugin();
     } else {
       figma.ui.postMessage("Select frame or group");
     }
   }
 
-  // figma.closePlugin();
+  closePlugin();
 };
--- src/ui.ts
+++ src/ui.ts
@@ -5,47 +5,52 @@ document.getElementById('create').onclick = () => {
     const input = document.getElementById('link');
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
     const link = input.value || 'https://www.miinto.pl/guide-k?sort=popular%20desc&page=368';
-
     const imgArray = [];
 
     if (imgChec.checked) {
-        fetch(proxyurl + link, {
-            method: 'GET',
-            headers: {
-                'X-Requested-With': 'XMLHttpRequest',
-                'Content-Type': 'application/json',
-                'Accept': '*/*'
-            }
-        }).then((response) => {
-            return response.json();
-        }).then((resJson) => {
-            resJson.products.list.forEach(item => {
-                fetch(proxyurl + item.photo_url).then(response => {
-                    return response.arrayBuffer()
-                }).then(a => {
-                    imgArray.push(new Uint8Array(a));
-                })
+
+        (function () {
+            fetch(proxyurl + link, {
+                method: 'GET',
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest',
+                    'Content-Type': 'application/json',
+                    'Accept': '*/*'
+                }
+            }).then((response) => {
+                return response.json();
+            }).then((resJson) => {
+                resJson.products.list.forEach(item => {
+                    fetch(proxyurl + item.photo_url).then(response => {
+                        return response.arrayBuffer()
+                    }).then(a => {
+                        imgArray.push(new Uint8Array(a));
+                    })
+                });
             });
-        });
+        })()
     }
 
-    const request = new XMLHttpRequest();
-    request.open('GET', proxyurl + link, true);
-    request.responseType = "text";
-    request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.setRequestHeader('Accept', '*/*');
+    (function () {
+        const request = new XMLHttpRequest();
+        request.open('GET', proxyurl + link, true);
+        request.responseType = "text";
+        request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.setRequestHeader('Accept', '*/*');
 
-    request.onload = () => {
-        parent.postMessage({
-            pluginMessage: {
-                response: request.response,
-                images: imgArray,
-                status: request.status
-            }
-        }, '*')
-    }
-    request.send()
+        request.onload = () => {
+            parent.postMessage({
+                pluginMessage: {
+                    response: request.response,
+                    images: imgArray,
+                    status: request.status
+                }
+            }, '*')
+        }
+        request.send()
+    })()
+
 }
 
 document.getElementById('cancel').onclick = () => {
